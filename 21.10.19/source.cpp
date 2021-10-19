2c97ccc0bec3f2e2b8d5aa992688189d5e36ba88
lsj1759 <lsj1759@naver.com>
2021-10-19 12:36:31 +0900

    학습 내용 추가

--- 21.10.19/source.cpp
+++ 21.10.19/source.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include "Editor.h"
+#include "Rectangle.h"
+#include "Triangle.h"
+#include "Circle.h"
 
 using namespace std;
 
 int main()
 {
+	Editor* editor = new Editor();
+	
+	editor->AddShape(new Triangle());
+	editor->AddShape(new Rectangle());
+	editor->AddShape(new Circle());
+
+	editor->Draw();
+
+	editor->Save();
+
+	delete editor;
+
 
 	return 0;
-}
+};
+
